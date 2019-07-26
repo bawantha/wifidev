2e2544da736ff33422286b617e1d0ac349155213
Bawantha Mihiran Rajapaksha <bawwa.16@cse.mrt.ac.lk>
2019-07-26 15:18:30 +0530

    Serilization and deserialization start

--- index.js
+++ index.js
@@ -5,6 +5,12 @@ const mongoose=require('mongoose');
 const keys=require('./config/keys');
 
 
+let _authaction="";             // call back url for authenticated user
+let _gatewayname="";            // GATEWAYNAME can be get as uniqure shoplocation
+let _tok="";                    // to authenticate user
+let _redir="";                  // redirect URL depend on platform( android, iOS)
+
+
 // impors routs
 const authRoutes=require('./routes/auth-routes');
 
@@ -27,7 +33,11 @@ app.use('/auth',authRoutes);
 
 // GET
 app.get('/',(req,res)=>{
-    res.render('home');
+    _authaction=req.query['authaction'];
+    _tok=req.query['tok'];
+    _gatewayname=req.query['gatewayname'];
+    _redir=req.query['redir'];
+    res.render('home',);
 })
 
 
--- services/passport-setup.js
+++ services/passport-setup.js
@@ -2,6 +2,16 @@ const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20');
 const keys = require('../config/keys');
 const Customer = require('../models/customer-model');
+
+passport.serializeUser((user, done) => {
+    done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+    Customer.findById(id).then((user) => {
+        done(null, user);
+    })
+});
 passport.use(
     new GoogleStrategy({
         callbackURL: '/auth/google/redir',
@@ -14,15 +24,19 @@ passport.use(
             googleID: profile.id
         }).then((currentCustomer) => {
             if (currentCustomer) {
+                done(null, currentCustomer);
+                // This is the point where user should let the internet access
                 // customer already registerd with our service
             } else {
                 // new Customer
                 new Customer({
-                    customerName:profile.displayName,
-                    googleID:profile.id
-                }).save().then((nc)=>{
-                   console.log ('new user created'+nc);
-                }) 
+                    customerName: profile.displayName,
+                    googleID: profile.id
+                }).save().then((nc) => {
+                    done(null,nc);
+                    // This is the where user should let the internet access
+                    console.log('new user created' + nc);
+                })
             }
         })
 
