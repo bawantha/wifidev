a5687e3aadc46373b0be2532bedd66b458f8c29e
Bawantha Mihiran Rajapaksha <bawwa.16@cse.mrt.ac.lk>
2019-07-29 12:45:39 +0530

    depcrication warining
    mongoose.set('useFindAndModify', false);

--- routes/auth-routes.js
+++ routes/auth-routes.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const passport = require('passport');
+const Customer=require('../models/customer-model');
 router.get("/login", (req, res) => {
   res.render('login');
 })
@@ -8,7 +9,20 @@ router.get("/login", (req, res) => {
 router.get("/", (req, res) => {
   if (req.user) {
     // db update function
-    // redirect to auth URL   
+    // redirect to auth URL
+    Customer.findByIdAndUpdate(
+      { _id: req.user.id }, 
+      { $push: { shopIDs:req.query.gatewayname} },
+     function (error, success) {
+           if (error) {
+               console.log(error);
+           } else {
+               console.log(success);
+           }
+       });
+
+
+//  
     res.send("Yolla bitchers")
   } else {
     res.redirect('/auth/login');
