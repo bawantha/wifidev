e92ee2a8bc80a4e59444a54d61f769184714e843
Bawantha Mihiran Rajapaksha <bawwa.16@cse.mrt.ac.lk>
2019-08-05 15:11:11 +0530

    new customer is saved to db

--- routes/middleware/numberAuth.js
+++ routes/middleware/numberAuth.js
@@ -2,27 +2,33 @@ let twilio_otp = require('../../services/twilio-otp');
 const customer = require('../../models/customer-model');
 //const conditional = require('express-conditional-middleware')
 //const nds = require('../config/nds');
-
+let pn;
+let _otp = twilio_otp.otp;
 module.exports = {
     numberAuth(req, res, next) {
-        var otp = twilio_otp.otp;
-
 
         if (typeof req.body.otp !== 'undefined') {
             // your code here
-            if (otp == req.body.otp) {
-                
-                // new customer
+            if (_otp == req.body.otp) {
+
+                // console.log(pn);
+                // new customer add
+                var newCustomer = new customer({
+                    customerPhoneNumber: pn + ""
+                })
+                newCustomer.save();
+
                 next();
             }
 
         } else {
 
-            const phoneNumber = req.body.phonenumber;
-            customer.findOne({ customerPhoneNumber: phoneNumber }, (err, data) => {
+            var _phoneNumber = req.body.phonenumber;
+            pn = _phoneNumber;
+            _otp = twilio_otp.otp;
+            customer.findOne({ customerPhoneNumber: _phoneNumber }, (err, data) => {
                 if (data == null) {
-                    //=otp;
-                    twilio_otp.sendOTP(phoneNumber, otp);
+                    twilio_otp.sendOTP(_phoneNumber, _otp);
                     res.render('otp-enter')
 
                 } else {
